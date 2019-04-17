adcbcd3c0c1169dc596b3d443075f6777818c58e
Jayasanka <jayasanka.sack@gmail.com>
2019-04-17 19:47:33 +0530

    completed the tutorial

--- part 2 - Middleware/react-redux-tutorial/src/js/store/index.js
+++ part 2 - Middleware/react-redux-tutorial/src/js/store/index.js
@@ -1,7 +1,9 @@
-import {createStore} from "redux"
-import rootReducer from "../reducers/index"
-
-const store = createStore(rootReducer);
-
-
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers/index";
+import { forbiddenWordsMiddleware } from "../middleware";
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    storeEnhancers(applyMiddleware(forbiddenWordsMiddleware))
+);
 export default store;
